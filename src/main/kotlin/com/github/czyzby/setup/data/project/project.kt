93ab4d79b597db70d16d62905bfb199d0d8f59d3
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-11-10 23:07:16 -0800

    Also part of 1.9.10.3: default to not using daemon, it seems to devour RAM

--- src/main/kotlin/com/github/czyzby/setup/data/project/project.kt
+++ src/main/kotlin/com/github/czyzby/setup/data/project/project.kt
@@ -30,7 +30,7 @@ class Project(val basic: BasicProjectData, val platforms: Map<String, Platform>,
     val files = mutableListOf<ProjectFile>()
     val rootGradle: RootGradleFile
     val properties = mutableMapOf(
-            "org.gradle.daemon" to "true",
+            "org.gradle.daemon" to "false", // was having very bad memory usage with daemon+Android
             "org.gradle.jvmargs" to "-Xms128m -Xmx512m",
             "org.gradle.configureondemand" to "false")
     val postGenerationTasks = mutableListOf<(Project) -> Unit>()
