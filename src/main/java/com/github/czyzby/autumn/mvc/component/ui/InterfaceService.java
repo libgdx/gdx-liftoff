cb4c5ca9dea71257298f72831fac53eb24c3e410
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-06-19 23:38:57 -0700

    Partial fix for HiDPI issues ( #17 )
    The font isn't exactly how I want it, but I can now resize the window to much larger and at least that way increase the text size. The window chrome is back; no way to avoid it if users need to resize the window. https://i.imgur.com/ETDOODl.png

--- src/main/java/com/github/czyzby/autumn/mvc/component/ui/InterfaceService.java
+++ src/main/java/com/github/czyzby/autumn/mvc/component/ui/InterfaceService.java
@@ -1,7 +1,5 @@
 package com.github.czyzby.autumn.mvc.component.ui;
 
-import java.util.Locale;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.files.FileHandle;
@@ -15,7 +13,7 @@ import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.I18NBundle;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.ObjectMap.Entry;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.github.czyzby.autumn.annotation.Destroy;
 import com.github.czyzby.autumn.annotation.Initiate;
@@ -23,21 +21,8 @@ import com.github.czyzby.autumn.annotation.Inject;
 import com.github.czyzby.autumn.mvc.component.asset.AssetService;
 import com.github.czyzby.autumn.mvc.component.i18n.LocaleService;
 import com.github.czyzby.autumn.mvc.component.sfx.MusicService;
-import com.github.czyzby.autumn.mvc.component.ui.action.ActionProvider;
-import com.github.czyzby.autumn.mvc.component.ui.action.ApplicationExitAction;
-import com.github.czyzby.autumn.mvc.component.ui.action.ApplicationPauseAction;
-import com.github.czyzby.autumn.mvc.component.ui.action.ApplicationResumeAction;
-import com.github.czyzby.autumn.mvc.component.ui.action.CommonActionRunnables;
-import com.github.czyzby.autumn.mvc.component.ui.action.DialogShowingAction;
-import com.github.czyzby.autumn.mvc.component.ui.action.MusicFadingAction;
-import com.github.czyzby.autumn.mvc.component.ui.action.ScreenTransitionAction;
-import com.github.czyzby.autumn.mvc.component.ui.controller.ViewController;
-import com.github.czyzby.autumn.mvc.component.ui.controller.ViewDialogController;
-import com.github.czyzby.autumn.mvc.component.ui.controller.ViewInitializer;
-import com.github.czyzby.autumn.mvc.component.ui.controller.ViewPauser;
-import com.github.czyzby.autumn.mvc.component.ui.controller.ViewRenderer;
-import com.github.czyzby.autumn.mvc.component.ui.controller.ViewResizer;
-import com.github.czyzby.autumn.mvc.component.ui.controller.ViewShower;
+import com.github.czyzby.autumn.mvc.component.ui.action.*;
+import com.github.czyzby.autumn.mvc.component.ui.controller.*;
 import com.github.czyzby.autumn.mvc.component.ui.controller.impl.AnnotatedViewDialogController;
 import com.github.czyzby.autumn.mvc.component.ui.controller.impl.StandardViewRenderer;
 import com.github.czyzby.autumn.mvc.component.ui.controller.impl.StandardViewResizer;
@@ -56,6 +41,9 @@ import com.github.czyzby.lml.parser.LmlParser;
 import com.github.czyzby.lml.parser.action.ActionContainer;
 import com.github.czyzby.lml.parser.action.ActorConsumer;
 import com.github.czyzby.lml.util.Lml;
+import com.github.czyzby.setup.config.Configuration;
+
+import java.util.Locale;
 
 /** Manages view controllers and a LML parser.
  *
@@ -617,7 +605,7 @@ public class InterfaceService {
         return new ObjectProvider<Viewport>() {
             @Override
             public Viewport provide() {
-                return new ScreenViewport();
+                return new StretchViewport(Configuration.WIDTH, Configuration.HEIGHT);
             }
         };
     }
--- src/main/kotlin/com/github/czyzby/setup/main.kt
+++ src/main/kotlin/com/github/czyzby/setup/main.kt
@@ -17,7 +17,8 @@ fun main(args: Array<String>) {
     config.setTitle("gdx-liftoff")
     config.setWindowedMode(Configuration.WIDTH, Configuration.HEIGHT)
     config.disableAudio(true)
-    config.setDecorated(false)
+//    config.setDecorated(false)
+    config.setResizable(true)
     config.setWindowIcon(*arrayOf(128, 64, 32, 16).map { "icons/libgdx$it.png" }.toTypedArray())
 
     try {
