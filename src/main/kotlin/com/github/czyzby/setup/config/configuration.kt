713869d2a54761c7141918c8438d92e2131625f0
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-11-01 17:05:15 -0800

    After some testing, release 1.9.12.0

--- src/main/kotlin/com/github/czyzby/setup/config/configuration.kt
+++ src/main/kotlin/com/github/czyzby/setup/config/configuration.kt
@@ -28,7 +28,7 @@ import com.kotcrab.vis.ui.widget.file.FileChooser
 @Component
 class Configuration {
     companion object {
-        const val VERSION = "1.9.11.5-SNAPSHOT"
+        const val VERSION = "1.9.12.0"
         const val WIDTH = 600
         const val HEIGHT = 700
         const val PREFERENCES_PATH = "gdx-liftoff-prefs"
