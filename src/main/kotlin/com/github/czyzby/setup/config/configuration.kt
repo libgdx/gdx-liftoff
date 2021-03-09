2d3e619ab3dbdd927e315adcb5c3e8b5743d510d
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-03-08 23:18:13 -0800

    Going to keep working on iOS issues...

--- src/main/kotlin/com/github/czyzby/setup/config/configuration.kt
+++ src/main/kotlin/com/github/czyzby/setup/config/configuration.kt
@@ -28,7 +28,7 @@ import com.kotcrab.vis.ui.widget.file.FileChooser
 @Component
 class Configuration {
     companion object {
-        const val VERSION = "1.9.14.2"
+        const val VERSION = "1.9.14.3-SNAPSHOT"
         const val WIDTH = 600
         const val HEIGHT = 700
         const val PREFERENCES_PATH = "gdx-liftoff-prefs"
