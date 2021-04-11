06a11cd90ac0be3d33340fbd5b92394b75a1030f
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-04-11 00:22:20 -0700

    Fix some deprecation warnings re: jar tasks.
    These would probably be more serious if a project updated to Gradle 7.

--- src/main/kotlin/com/github/czyzby/setup/data/platforms/server.kt
+++ src/main/kotlin/com/github/czyzby/setup/data/platforms/server.kt
@@ -42,11 +42,15 @@ dependencies {
 ${joinDependencies(dependencies)}}
 
 jar {
-	archiveBaseName = "${'$'}{appName}-server"
+	archiveBaseName.set(appName)
 	dependsOn configurations.runtimeClasspath
 	from { configurations.runtimeClasspath.collect { it.isDirectory() ? it : zipTree(it) } } 
 	manifest {
 		attributes 'Main-Class': project.mainClassName
 	}
-}"""
+	doLast {
+		file(archiveFile).setExecutable(true, false)
+	}
+}
+"""
 }
