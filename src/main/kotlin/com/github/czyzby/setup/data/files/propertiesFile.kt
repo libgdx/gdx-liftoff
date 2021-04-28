778bfd12e2152c12dd7b7f4cbd52c75fd6e6e2a7
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-04-28 16:13:45 -0700

    Add trailing newlines for that picky tooling.

--- src/main/kotlin/com/github/czyzby/setup/data/files/propertiesFile.kt
+++ src/main/kotlin/com/github/czyzby/setup/data/files/propertiesFile.kt
@@ -10,7 +10,7 @@ class PropertiesFile(val properties: Map<String, String>) : ProjectFile {
     override val path = "gradle.properties"
 
     override fun save(destination: FileHandle) {
-        val content = properties.map { it.key + "=" + it.value }.joinToString(separator = "\n")
+        val content = properties.map { it.key + "=" + it.value }.joinToString(separator = "\n", postfix = "\n")
         destination.child(path).writeString(content, false, "UTF-8")
     }
 }
