11a26eef648695cabc6f88f6731345b5fe5bc3e3
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-08-03 16:15:40 -0700

    Make a version that uses a VisLabel for the scrollable output, but it isn't selectable...

--- src/main/kotlin/com/github/czyzby/setup/views/widgets/scrollableTextArea.kt
+++ src/main/kotlin/com/github/czyzby/setup/views/widgets/scrollableTextArea.kt
@@ -1,37 +1,35 @@
 package com.github.czyzby.setup.views.widgets
 
-import com.badlogic.gdx.scenes.scene2d.ui.TextArea
-import com.badlogic.gdx.scenes.scene2d.ui.TextField
+import com.badlogic.gdx.scenes.scene2d.Actor
 import com.github.czyzby.kiwi.util.common.Strings
 import com.github.czyzby.lml.parser.LmlParser
 import com.github.czyzby.lml.parser.impl.tag.builder.TextLmlActorBuilder
+import com.github.czyzby.lml.parser.tag.LmlActorBuilder
 import com.github.czyzby.lml.parser.tag.LmlTag
 import com.github.czyzby.lml.parser.tag.LmlTagProvider
-import com.github.czyzby.lml.vis.parser.impl.tag.BasicVisTextAreaLmlTag
-import com.github.czyzby.lml.vis.parser.impl.tag.VisTextAreaLmlTag
-import com.kotcrab.vis.ui.VisUI
+import com.github.czyzby.lml.vis.parser.impl.tag.VisLabelLmlTag
+import com.kotcrab.vis.ui.widget.VisLabel
 import com.kotcrab.vis.ui.widget.VisTextArea
-import com.kotcrab.vis.ui.widget.VisTextField
 
 /** Custom [VisTextArea] used to display logs in the generation prompt. Supports embedding in scroll pane by calculating
  * required space needed for current text.
 
  * @author Kotcrab
  */
-class ScrollableTextArea(text: String, styleName: String) : TextArea(text, VisUI.getSkin(), styleName) {
+class ScrollableTextArea(text: String, styleName: String) : VisLabel(text, styleName) {
     init {
         style.font.data.markupEnabled = true
-        isDisabled = false;
+        wrap = true;
     }
     override fun getPrefWidth(): Float {
         return width
     }
 
     override fun getPrefHeight(): Float {
-        return lines * style.font.lineHeight
+        return glyphLayout.height
     }
 
-    override fun setText(str: String) {
+    override fun setText(str: CharSequence) {
         // TextArea seems to have problem when '\r\n' (Windows style) is used as line ending, as it treats it as two
         // lines. Although example templates files are using '\n', Git (when cloning the repository) may replace them
         // with '\r\n' on Windows.
@@ -53,8 +51,13 @@ class ScrollableTextArea(text: String, styleName: String) : TextArea(text, VisUI
      * @author Kotcrab
      */
     class ScrollableTextAreaLmlTag(parser: LmlParser, parentTag: LmlTag, rawTagData: StringBuilder) :
-            BasicVisTextAreaLmlTag(parser, parentTag, rawTagData) {
-        override fun getNewInstanceOfTextField(textBuilder: TextLmlActorBuilder): TextField =
-                ScrollableTextArea(textBuilder.text, textBuilder.styleName)
+            VisLabelLmlTag(parser, parentTag, rawTagData) {
+        override fun getNewInstanceOfActor(builder: LmlActorBuilder?): Actor {
+            val textBuilder = builder as TextLmlActorBuilder
+            return ScrollableTextArea(textBuilder.getText(), builder.getStyleName())
+
+        }
     }
+    //        override fun getNewInstanceOfTextField(textBuilder: TextLmlActorBuilder): Label =
+    //                ScrollableTextArea(textBuilder.text, textBuilder.styleName)
 }
