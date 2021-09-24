ba606180169f596ec2adbfbf1f482664f399010b
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-24 15:42:13 -0700

    GWT projects default to resizable with no padding, instead of some small amount.

--- src/main/kotlin/com/github/czyzby/setup/data/templates/template.kt
+++ src/main/kotlin/com/github/czyzby/setup/data/templates/template.kt
@@ -142,10 +142,14 @@ public class GwtLauncher extends GwtApplication {""" + (if(LibGdxVersion.parseLi
 else """
 		@Override
 		public GwtApplicationConfiguration getConfig () {
-			// Resizable application, uses available space in browser
-			return new GwtApplicationConfiguration(true);
-			// Fixed size application:
-			//return new GwtApplicationConfiguration(480, 320);
+			// Resizable application, uses available space in browser with no padding:
+			GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
+			cfg.padVertical = 0;
+			cfg.padHorizontal = 0;
+			return cfg;
+			// If you want a fixed size application, comment out the above resizable section,
+			// and uncomment below:
+			//return new GwtApplicationConfiguration($width, $height);
 		}
 """) + 
 """
