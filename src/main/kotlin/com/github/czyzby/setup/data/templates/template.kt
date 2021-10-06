8c60adf53113f63adb9f97933a1590d844890457
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-10-06 13:22:08 -0700

    Extra limit on FPS for LWJGL3, for buggy machines that don't understand Vsync.

--- src/main/kotlin/com/github/czyzby/setup/data/templates/template.kt
+++ src/main/kotlin/com/github/czyzby/setup/data/templates/template.kt
@@ -267,6 +267,12 @@ public class Lwjgl3Launcher {
 	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
 		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
 		configuration.setTitle("${project.basic.name}");
+		configuration.useVsync(true);
+		//// Limits FPS to the refresh rate of the currently active monitor.
+		configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+		//// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
+		//// useful for testing performance, but can also be very stressful to some hardware.
+		//// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
 		configuration.setWindowedMode(${width}, ${height});
 		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
 		return configuration;
