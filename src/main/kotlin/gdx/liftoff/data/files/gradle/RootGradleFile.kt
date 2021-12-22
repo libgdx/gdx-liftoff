a3d3ce02c1888dfb1173200c8ac573b9412fd7e3
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-12-22 14:58:42 -0800

    FCS back down to working version, Gradle up.
    Gradle is now at 7.3.3, and the methods to fully eliminate vulnerable versions of log4j recommended by Gradle's authors are also exercised in generated projects (because they are somewhat onerous to do by hand).

--- src/main/kotlin/gdx/liftoff/data/files/gradle/RootGradleFile.kt
+++ src/main/kotlin/gdx/liftoff/data/files/gradle/RootGradleFile.kt
@@ -25,7 +25,18 @@ class RootGradleFile(val project: Project) : GradleFile("") {
 ${buildRepositories.joinToString(separator = "\n") { "		$it" }}
 	}
 	dependencies {
-${joinDependencies(buildDependencies, type = "classpath", tab = "		")}	}
+${joinDependencies(buildDependencies, type = "classpath", tab = "		")}
+		// This follows advice from https://blog.gradle.org/log4j-vulnerability
+		constraints {
+			classpath("org.apache.logging.log4j:log4j-core") {
+				version {
+					strictly("[2.17, 3[")
+					prefer("2.17.0")
+				}
+				because("CVE-2021-44228, CVE-2021-45046, CVE-2021-45105: Log4j vulnerable to remote code execution and other critical security vulnerabilities")
+			}
+		}
+	}
 }
 
 allprojects {
@@ -43,6 +54,19 @@ ${plugins.joinToString(separator = "\n") { "	apply plugin: '$it'" }}
 	compileJava {
 		options.incremental = true
 	}
+	dependencies {
+		// This follows advice from https://blog.gradle.org/log4j-vulnerability
+		constraints {
+			implementation("org.apache.logging.log4j:log4j-core") {
+				version {
+					strictly("[2.17, 3[")
+					prefer("2.17.0")
+				}
+				because("CVE-2021-44228, CVE-2021-45046, CVE-2021-45105: Log4j vulnerable to remote code execution and other critical security vulnerabilities")
+			}
+		}
+	}
+
 }
 
 subprojects {
@@ -54,7 +78,7 @@ subprojects {
 		mavenLocal()
 		gradlePluginPortal()
 		maven { url 'https://oss.sonatype.org/content/repositories/snapshots/' }
-        maven { url 'https://s01.oss.sonatype.org/content/repositories/snapshots/' }
+		maven { url 'https://s01.oss.sonatype.org/content/repositories/snapshots/' }
 		maven { url 'https://jitpack.io' }
 	}
 }
