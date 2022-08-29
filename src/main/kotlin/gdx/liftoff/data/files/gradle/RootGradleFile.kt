a041234ec6640186336aa00969a21f182637c263
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-08-28 19:27:02 -0700

    Update Kotlin, log4j restriction, options typo.
    Of these, the typo in options is likely the biggest issue, but I don't know if removing the extraneous space in the jpackage config for lwjgl3/build.gradle will allow it to build correctly on Mac.

--- src/main/kotlin/gdx/liftoff/data/files/gradle/RootGradleFile.kt
+++ src/main/kotlin/gdx/liftoff/data/files/gradle/RootGradleFile.kt
@@ -30,8 +30,8 @@ ${joinDependencies(buildDependencies, type = "classpath", tab = "		")}
 		constraints {
 			classpath("org.apache.logging.log4j:log4j-core") {
 				version {
-					strictly("[2.17, 3[")
-					prefer("2.17.0")
+					strictly("[2.18, 3[")
+					prefer("2.18.0")
 				}
 				because("CVE-2021-44228, CVE-2021-45046, CVE-2021-45105: Log4j vulnerable to remote code execution and other critical security vulnerabilities")
 			}
@@ -59,8 +59,8 @@ ${plugins.joinToString(separator = "\n") { "	apply plugin: '$it'" }}
 		constraints {
 			implementation("org.apache.logging.log4j:log4j-core") {
 				version {
-					strictly("[2.17, 3[")
-					prefer("2.17.0")
+					strictly("[2.18, 3[")
+					prefer("2.18.0")
 				}
 				because("CVE-2021-44228, CVE-2021-45046, CVE-2021-45105: Log4j vulnerable to remote code execution and other critical security vulnerabilities")
 			}
--- src/main/kotlin/gdx/liftoff/data/platforms/Android.kt
+++ src/main/kotlin/gdx/liftoff/data/platforms/Android.kt
@@ -181,8 +181,8 @@ ${joinDependencies(nativeDependencies, "natives")}
 	constraints {
 		implementation("org.apache.logging.log4j:log4j-core") {
 			version {
-				strictly("[2.17, 3[")
-				prefer("2.17.0")
+				strictly("[2.18, 3[")
+				prefer("2.18.0")
 			}
 			because("CVE-2021-44228, CVE-2021-45046, CVE-2021-45105: Log4j vulnerable to remote code execution and other critical security vulnerabilities")
 		}
