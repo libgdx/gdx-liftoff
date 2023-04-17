db743dfcfc799cabd7c4b8677dba5ac1fa29b42c
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-04-17 00:31:14 -0700

    Improve non-A-Z name script to avoid admin.
    Now it doesn't need admin because it doesn't create a folder in C:\ -- it reuses the existing path at what is usually "C:\ProgramData", which hopefully doesn't have non-A-Z characters in it due to localization...

--- etc/gdx-liftoff-non-a-z-name-VERSION.bat
+++ etc/gdx-liftoff-non-a-z-name-VERSION.bat
@@ -1,2 +1,2 @@
 @echo OFF
-(IF NOT EXIST "C:\gdx-liftoff\.temp" MKDIR "C:\gdx-liftoff\.temp") && SET TMP=C:\gdx-liftoff\.temp && (IF NOT EXIST "C:\gdx-liftoff\gdx-liftoff-VERSION.jar" COPY gdx-liftoff-VERSION.jar C:\gdx-liftoff\gdx-liftoff-VERSION.jar) && START /D C:\gdx-liftoff javaw -jar gdx-liftoff-VERSION.jar
+(IF NOT EXIST "%ProgramData%/gdx-liftoff/temp" MKDIR "%ProgramData%/gdx-liftoff/temp") && SET TMP=temp && copy gdx-liftoff-VERSION.jar "%ProgramData%/gdx-liftoff\gdx-liftoff-VERSION.jar" && (START /D "%ProgramData%/gdx-liftoff" javaw -jar gdx-liftoff-VERSION.jar)
