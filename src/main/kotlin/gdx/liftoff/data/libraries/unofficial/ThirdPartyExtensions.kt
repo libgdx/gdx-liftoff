f361c8c994c775a5d7aedd372fce774c8278679e
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-08-17 04:11:32 -0700

    Update to Gradle 8.3 .

--- src/main/kotlin/gdx/liftoff/data/libraries/unofficial/ThirdPartyExtensions.kt
+++ src/main/kotlin/gdx/liftoff/data/libraries/unofficial/ThirdPartyExtensions.kt
@@ -364,7 +364,7 @@ class TypingLabel : ThirdPartyExtension() {
 @Extension
 class TextraTypist : ThirdPartyExtension() {
   override val id = "textratypist"
-  override val defaultVersion = "0.8.3"
+  override val defaultVersion = "0.9.0"
   override val url = "https://github.com/tommyettinger/textratypist"
   override val group = "com.github.tommyettinger"
   override val name = "textratypist"
