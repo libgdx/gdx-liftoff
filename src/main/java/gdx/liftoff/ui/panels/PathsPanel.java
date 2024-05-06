a0e8989aa9537c2e2f0970c483de432ee5461d05
raeleus <raymond.ray3k@gmail.com>
2024-05-06 07:31:51 -0700

    Set the appropriate initialFolder when using pickfolder

--- src/main/java/gdx/liftoff/ui/panels/PathsPanel.java
+++ src/main/java/gdx/liftoff/ui/panels/PathsPanel.java
@@ -15,7 +15,6 @@ import gdx.liftoff.ui.data.UserData;
 import static gdx.liftoff.Main.*;
 
 //todo:add an error label
-//todo:default path should be what's in the field
 /**
  * Table that displays the project path and the android sdk path if android is selected as a platform
  */
@@ -33,21 +32,26 @@ public class PathsPanel extends Table implements Panel {
 
         //project field
         TextButton projectFieldButton = addField(UserData.projectPath);
-        onChange(projectFieldButton, () -> Main.pickDirectory(Gdx.files.absolute(Gdx.files.getExternalStoragePath()), new FileChooserAdapter() {
-            @Override
-            public void canceled() {
+        onChange(projectFieldButton, () -> {
+            FileHandle initialFolder = Gdx.files.absolute(Gdx.files.getExternalStoragePath());
+            if (UserData.projectPath != null && !UserData.projectPath.isEmpty()) initialFolder = Gdx.files.absolute(UserData.projectPath);
 
-            }
+            Main.pickDirectory(initialFolder, new FileChooserAdapter() {
+                @Override
+                public void canceled() {
 
-            @Override
-            public void selected(Array<FileHandle> files) {
-                if (files.size > 0) {
-                    String path = files.first().path();
-                    projectFieldButton.setText(path);
-                    UserData.projectPath = path;
                 }
-            }
-        }));
+
+                @Override
+                public void selected(Array<FileHandle> files) {
+                    if (files.size > 0) {
+                        String path = files.first().path();
+                        projectFieldButton.setText(path);
+                        UserData.projectPath = path;
+                    }
+                }
+            });
+        });
 
         if (UserData.platforms.contains(prop.getProperty("android"), false)) {
             //android label
@@ -58,23 +62,28 @@ public class PathsPanel extends Table implements Panel {
 
             //android field
             TextButton androidFieldButton = addField(UserData.androidPath);
-            onChange(androidFieldButton, () -> Main.pickDirectory(Gdx.files.absolute(Gdx.files.getExternalStoragePath()), new FileChooserAdapter() {
-                @Override
-                public void canceled() {
+            onChange(androidFieldButton, () -> {
+                FileHandle initialFolder = Gdx.files.absolute(Gdx.files.getExternalStoragePath());
+                if (UserData.androidPath != null && !UserData.androidPath.isEmpty()) initialFolder = Gdx.files.absolute(UserData.androidPath);
 
-                }
+                Main.pickDirectory(initialFolder, new FileChooserAdapter() {
+                    @Override
+                    public void canceled() {
 
-                @Override
-                public void selected(Array<FileHandle> files) {
-                    if (files.size > 0) {
-                        String path = files.first().path();
-                        androidFieldButton.setText(path);
-                        UserData.androidPath = path;
-                        pref.putString("AndroidSdk", path);
-                        pref.flush();
                     }
-                }
-            }));
+
+                    @Override
+                    public void selected(Array<FileHandle> files) {
+                        if (files.size > 0) {
+                            String path = files.first().path();
+                            androidFieldButton.setText(path);
+                            UserData.androidPath = path;
+                            pref.putString("AndroidSdk", path);
+                            pref.flush();
+                        }
+                    }
+                });
+            });
         }
     }
 
