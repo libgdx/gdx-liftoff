2198d8dc73579d11ef2431297f6977336b828d08
beespoke-ltd <dangelodeniro95@gmail.com>
2025-05-01 21:38:57 -0600

    Movement Helper

---
+++ core/src/main/java/com/denireaux/fallingsand/helpers/MovementHelper.java
@@ -0,0 +1,18 @@
+package com.denireaux.fallingsand.helpers;
+
+import com.denireaux.fallingsand.particletypes.Particle;
+
+public class MovementHelper {
+
+    public static boolean canMoveDown(Particle[][] grid, int x, int y) {
+        return y > 0 && grid[x][y - 1] == null;
+    }
+
+    public static boolean canMoveDownLeft(Particle[][] grid, int x, int y) {
+        return x > 0 && y > 0 && grid[x - 1][y - 1] == null && grid[x - 1][y] == null;
+    }
+
+    public static boolean canMoveDownRight(Particle[][] grid, int x, int y) {
+        return x < grid.length - 1 && y > 0 && grid[x + 1][y - 1] == null && grid[x + 1][y] == null;
+    }
+}
