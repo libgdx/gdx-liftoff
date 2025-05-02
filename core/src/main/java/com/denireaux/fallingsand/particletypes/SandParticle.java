cca30dab874f3682b9714cd732ea77ed9f6c399a
beespoke-ltd <dangelodeniro95@gmail.com>
2025-05-01 21:38:17 -0600

    Implemented Down-Diagnal Movement for SandParticle and created MovementHelper class

--- core/src/main/java/com/denireaux/fallingsand/particletypes/SandParticle.java
+++ core/src/main/java/com/denireaux/fallingsand/particletypes/SandParticle.java
@@ -1,9 +1,42 @@
 package com.denireaux.fallingsand.particletypes;
 
+import com.denireaux.fallingsand.helpers.MovementHelper;
+import com.denireaux.fallingsand.utils.utils;
+
 public class SandParticle extends Particle {
     public SandParticle(int x, int y) {
         super(x, y);
     }
+
+    // @Override
+    // public void update(float gravity, Particle[][] grid) {
+    //     velocity += gravity;
+
+    //     if (y <= 0) return;
+
+    //     Particle[] particles = getSurroundingParticles(grid);
+    //     Particle particleLeft = particles[0];
+    //     Particle particleRight = particles[1];
+    //     Particle particleBelow = particles[3];
+
+    //     // Down
+    //     if (y > 0 && grid[x][y - 1] == null) {
+    //         moveDown(grid, x, y - 1);
+    //         return;
+    //     }
+
+    //     // Down-right
+    //     if (x + 1 < grid.length && y - 1 >= 0 && particleRight == null && grid[x + 1][y - 1] == null) {
+    //         moveDownRight(grid);
+    //         return;
+    //     }
+
+    //     // Down-left
+    //     if (x - 1 >= 0 && y - 1 >= 0 && particleLeft == null && grid[x - 1][y - 1] == null) {
+    //         moveDownLeft(grid);
+    //         return;
+    //     }
+    // }
 
     @Override
     public void update(float gravity, Particle[][] grid) {
@@ -11,25 +44,39 @@ public class SandParticle extends Particle {
 
         if (y <= 0) return;
 
-        Particle[] particles = getSurroundingParticles(grid);
-        Particle particleLeft = particles[0];
-        Particle particleRight = particles[1];
-        Particle particleBelow = particles[3];
+        // Particle[] particles = getSurroundingParticles(grid);
+        // Particle particleLeft = particles[0];
+        // Particle particleRight = particles[1];
+        // Particle particleBelow = particles[3];
 
-        // Down
-        if (y > 0 && grid[x][y - 1] == null) {
+        // Straight down
+        if (y > 0 && MovementHelper.canMoveDown(grid, x, y)) {
             moveDown(grid, x, y - 1);
             return;
         }
 
-        // Down-right
-        if (x + 1 < grid.length && y - 1 >= 0 && particleRight == null && grid[x + 1][y - 1] == null) {
+        // Both diagonals available
+        boolean canDownLeft = MovementHelper.canMoveDownLeft(grid, x, y);
+        boolean canDownRight = MovementHelper.canMoveDownRight(grid, x, y);
+
+        boolean movingDownLeft = utils.getRandomBoolean();
+
+        if (canDownLeft && canDownRight) {
+            if (movingDownLeft) {
+                moveDownLeft(grid);
+                return;
+            } else {
+                moveDownRight(grid);
+                return;
+            }
+        }
+        
+        if (canDownRight && !canDownLeft) {
             moveDownRight(grid);
             return;
         }
-
-        // Down-left
-        if (x - 1 >= 0 && y - 1 >= 0 && particleLeft == null && grid[x - 1][y - 1] == null) {
+        
+        if (canDownLeft && !canDownRight) {
             moveDownLeft(grid);
             return;
         }
@@ -54,5 +101,17 @@ public class SandParticle extends Particle {
         grid[x - 1][y - 1] = this;
         x--;
         y--;
+    }
+
+    private void moveLeft(Particle[][] grid) {
+        grid[x][y] = null;
+        grid[x - 1][y] = this;
+        x--;
+    }
+
+    private void moveRight(Particle[][] grid) {
+        grid[x][y] = null;
+        grid[x + 1][y] = this;
+        x++;
     }
 }
