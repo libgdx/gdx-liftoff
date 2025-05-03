01e3fc3590ea28ca62a8a53348051f4d1dc7e71a
denireaux <dangelo.l.deniro@gmail.com>
2025-05-02 22:20:14 -0600

    Method and class docstrings for SandParticle

--- core/src/main/java/com/denireaux/fallingsand/particletypes/SandParticle.java
+++ core/src/main/java/com/denireaux/fallingsand/particletypes/SandParticle.java
@@ -3,43 +3,28 @@ package com.denireaux.fallingsand.particletypes;
 import com.denireaux.fallingsand.helpers.MovementHelper;
 import com.denireaux.fallingsand.utils.utils;
 
+
+/**
+ * Represents a sand particle in the falling sand simulation.
+ * 
+ * Sand particles are affected by gravity and fall straight down if possible.
+ * When blocked, they attempt to move diagonally left or right.
+ * 
+ * This class handles basic gravity application and movement logic.
+ * @author D'Angelo L. DeNiro
+ */
 public class SandParticle extends Particle {
     public SandParticle(int x, int y) {
         super(x, y);
     }
 
-    // @Override
-    // public void update(float gravity, Particle[][] grid) {
-    //     velocity += gravity;
-
-    //     if (y <= 0) return;
-
-    //     Particle[] particles = getSurroundingParticles(grid);
-    //     Particle particleLeft = particles[0];
-    //     Particle particleRight = particles[1];
-    //     Particle particleBelow = particles[3];
-
-    //     // Down
-    //     if (y > 0 && grid[x][y - 1] == null) {
-    //         moveDown(grid, x, y - 1);
-    //         return;
-    //     }
-
-    //     // Down-right
-    //     if (x + 1 < grid.length && y - 1 >= 0 && particleRight == null && grid[x + 1][y - 1] == null) {
-    //         moveDownRight(grid);
-    //         return;
-    //     }
-
-    //     // Down-left
-    //     if (x - 1 >= 0 && y - 1 >= 0 && particleLeft == null && grid[x - 1][y - 1] == null) {
-    //         moveDownLeft(grid);
-    //         return;
-    //     }
-    // }
-
-    // TODO: Fix invisible sand
-    // For whatever reason the sand when spawned, is completely invisible
+    /**
+     * Updates the sand particle's position based on gravity and surrounding particles.
+     * This simulates falling behavior and diagonal sliding if the path directly below is blocked.
+     *
+     * @param gravity the amount of gravity to apply to the particle's vertical velocity
+     * @param grid the 2D array representing the current particle grid
+     */
     @Override
     public void update(float gravity, Particle[][] grid) {
         gravity *= 6;
@@ -92,7 +77,13 @@ public class SandParticle extends Particle {
         velocity -= moveSteps;
     }
     
-
+    /**
+     * Moves the particle straight down in the grid.
+     *
+     * @param grid the 2D particle array
+     * @param newX the X-coordinate to move to
+     * @param newY the Y-coordinate to move to
+     */
     private void moveDown(Particle[][] grid, int newX, int newY) {
         grid[newX][newY] = this;
         grid[x][y] = null;
@@ -100,6 +91,11 @@ public class SandParticle extends Particle {
         y = newY;
     }
 
+    /**
+     * Moves the particle down and to the right in the grid.
+     *
+     * @param grid the 2D particle array
+     */
     private void moveDownRight(Particle[][] grid) {
         grid[x][y] = null;
         grid[x + 1][y - 1] = this;
@@ -107,6 +103,11 @@ public class SandParticle extends Particle {
         y--;
     }
 
+    /**
+     * Moves the particle down and to the left in the grid.
+     *
+     * @param grid the 2D particle array
+     */
     private void moveDownLeft(Particle[][] grid) {
         grid[x][y] = null;
         grid[x - 1][y - 1] = this;
@@ -114,12 +115,22 @@ public class SandParticle extends Particle {
         y--;
     }
 
+    /**
+     * Moves the particle one cell to the left.
+     *
+     * @param grid the 2D particle array
+     */
     private void moveLeft(Particle[][] grid) {
         grid[x][y] = null;
         grid[x - 1][y] = this;
         x--;
     }
 
+    /**
+     * Moves the particle one cell to the right.
+     *
+     * @param grid the 2D particle array
+     */
     private void moveRight(Particle[][] grid) {
         grid[x][y] = null;
         grid[x + 1][y] = this;
