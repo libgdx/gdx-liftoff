01c09251d448dcae1a90c94a591dac89bfd49ec5
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-06-21 02:51:51 -0700

    Revert the metalangle changes... They do need...
    ... to be one-or-the-other.

--- src/main/kotlin/gdx/liftoff/data/platforms/IOS.kt
+++ src/main/kotlin/gdx/liftoff/data/platforms/IOS.kt
@@ -207,17 +207,19 @@ class IOSGradleFile(
     dependencies.add("project(':${Core.ID}')")
     addDependency("com.mobidevelop.robovm:robovm-rt:\$robovmVersion")
     addDependency("com.mobidevelop.robovm:robovm-cocoatouch:\$robovmVersion")
-    // Thanks to etherjoe for verifying that this line is needed!
-    addDependency("com.badlogicgames.gdx:gdx-backend-robovm:\$gdxVersion")
     // The check for greater than 1.14.2 assumes https://github.com/libgdx/libgdx/issues/7422 gets merged.
-    if (GdxVersion.parseGdxVersion(project.advanced.gdxVersion) != null &&
-      (
-        GdxVersion.parseGdxVersion(project.advanced.gdxVersion)!! >= GdxVersion(1, 12, 0) &&
-          GdxVersion.parseGdxVersion(project.advanced.gdxVersion)!! <= GdxVersion(1, 14, 2)
-      )
-    ) {
-      addDependency("com.badlogicgames.gdx:gdx-backend-robovm-metalangle:\$gdxVersion")
-    }
+    addDependency(
+      "com.badlogicgames.gdx:gdx-backend-robovm${if (GdxVersion.parseGdxVersion(project.advanced.gdxVersion) != null &&
+        (
+          GdxVersion.parseGdxVersion(project.advanced.gdxVersion)!! < GdxVersion(1, 12, 0) ||
+            GdxVersion.parseGdxVersion(project.advanced.gdxVersion)!! > GdxVersion(1, 14, 2)
+        )
+      ) {
+        ""
+      } else {
+        "-metalangle"
+      }}:\$gdxVersion",
+    )
     addDependency("com.badlogicgames.gdx:gdx-platform:\$gdxVersion:natives-ios")
   }
 
